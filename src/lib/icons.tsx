ff55bf8e12f535a483b05e12219958d33b1916fe
Ahmed Saleem Shaikh <98047091+AhmedShaykh@users.noreply.github.com>
2025-01-28 20:16:00 +0500

    Delete src/lib/icons.tsx

--- src/lib/icons.tsx
+++
@@ -1,5 +0,0 @@
-// export const home = <i className="fa-solid fa-house"> </i>;
-
-// export const arrowAngleRight = <i className="fa-solid fa-angle-right"> </i>;
-
-// export const volumeHigh = <i className="fa-solid fa-volume-up"> </i>;
